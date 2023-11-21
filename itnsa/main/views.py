f47761322ffe5b7c7860a51bcbf885413e642b21
hdaojin <hdaojin@hotmail.com>
2023-11-21 13:37:52 +0800

    pages is OK

--- itnsa/main/views.py
+++ itnsa/main/views.py
@@ -1,15 +1,25 @@
-from flask import  render_template
+from flask import  render_template, url_for, current_app
 
 from . import main
 
+@main.context_processor
+def inject_nav():
+    nav = [
+        {'name': 'Home', 'url': url_for('main.index')},
+        {'name': 'About', 'url': url_for('main.about')}
+    ]
+
+    return dict(nav=nav)
+
 @main.route('/')
 def index():
-    return render_template('pages/index.html')
+    content = "This is the home page"
+    return render_template('pages/index.html', content=content, title="Home")
 
 @main.route('/about')
 def about():
     content = "This is the about page"
-    return render_template('pages/about.html', content=content)
+    return render_template('pages/about.html', content=content, title="About")
 
 
 
