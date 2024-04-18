92dd6a2a98246d20e22bef962a37cfe52f512c49
hdaojin <hdaojin@hotmail.com>
2024-04-18 10:15:51 +0800

    bugfix: DateField 默认值不刷新的问题

--- itnsa/traininglog/forms.py
+++ itnsa/traininglog/forms.py
@@ -5,9 +5,14 @@ from wtforms import StringField, DateField, RadioField, SelectField, SubmitField
 from wtforms.validators import DataRequired, Length, NumberRange, Optional
 from config import app_config
 
+# DateField默认值设置为datetime.today()。这个函数只会在模块加载时计算一次，因此如果你的应用是长时间运行的，这个日期不会更新。为了确保每次加载表单时日期都是最新的，你需要将默认值设置为一个函数，这样每次调用时都会重新计算。
+
+def today_date():
+    return datetime.today()
+
 class TrainingLogUploadForm(FlaskForm):
     module = SelectField('模块', validators=[DataRequired()]) # Dynamic choices in view function
-    date = DateField('日期', validators=[DataRequired()], default=datetime.today(), description="*特别注意: 请选择日志对应的训练日期，不是日志上传的日期")
+    date = DateField('日期', validators=[DataRequired()], default=today_date, description="*特别注意: 请选择日志对应的训练日期，不是日志上传的日期")
     task = StringField('任务', validators=[DataRequired(), Length(max=30, message='任务长度不能超过30个字符')], description='任务长度不能超过30个字符')
     type = RadioField('类型', validators=[DataRequired()]) # Dynamic choices in view function
     file = FileField('上传文件', validators=[FileRequired(), 
